0d03bc1d482467ccada5600c03d50d8956c31d3f
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2022-06-07 19:57:20 -0500

    I forgot commit this at creation xd

---
+++ src/main/java/xyz/saulmmbp/CrudDatabaseProjectApplication.java
@@ -0,0 +1,13 @@
+package xyz.saulmmbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDatabaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDatabaseProjectApplication.class, args);
+	}
+
+}
