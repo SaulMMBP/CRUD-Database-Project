3b03830ec232f449884e696183c73147915b3879
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2022-06-07 19:04:15 -0500

    Creacion de proyecto inicial

---
+++ src/test/java/xyz/saulmmbp/CrudDatabaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.saulmmbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDatabaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
